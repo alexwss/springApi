e30ec90079d0f1dfd09f4983384fe6d9b89b2d10
alexwss <alexwssilva@gmail.com>
2019-04-13 16:39:02 -0300

    oi

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -1,17 +1,15 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.input.TopicSearchInputDto;
 import br.com.alura.forum.controller.dto.output.TopicBriefOutputDto;
-import br.com.alura.forum.model.Category;
-import br.com.alura.forum.model.Course;
-import br.com.alura.forum.model.User;
 import br.com.alura.forum.model.topic.domain.Topic;
 import br.com.alura.forum.repository.TopicRepository;
 
@@ -22,7 +20,7 @@ public class TopicController {
 	private TopicRepository topicRepository;
 
 	@GetMapping(value="/api/topics", produces= MediaType.APPLICATION_JSON_VALUE)
-	public List<TopicBriefOutputDto> listTopics(){
+	public List<TopicBriefOutputDto> listTopics(TopicSearchInputDto topicSearch){
 		/*1
 		Category subcategory = new Category("Java", new Category("Programação"));
 		Course course = new Course("Java e JSF", subcategory);
@@ -32,9 +30,8 @@ public class TopicController {
 		
 		List<Topic> topics = Arrays.asList(topic, topic, topic);*/
 		
-		
+		Specification<Topic> topicSearchSpecification = topicSearch.build();
 		List<Topic> topics = topicRepository.findAll(topicSearchSpecification);
-		
 		
 		return  TopicBriefOutputDto.listFromTopics(topics);
 		
